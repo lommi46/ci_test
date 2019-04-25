4a586e25f2016b9aec2c72717923809ac1e3a908
TimHffmnn <tim.hoffmann01@sap.com>
2019-04-25 11:02:30 +0200

    .

---
+++ java_app/common/src/test/java/com/company/ci_test/java_app/Junit_Test.java
@@ -0,0 +1,13 @@
+package com.company.ci_test.java_app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Junit_Test {
+
+	@Test
+	public void test() {
+	 Assert.assertEquals(1, 1);
+	}
+
+}
